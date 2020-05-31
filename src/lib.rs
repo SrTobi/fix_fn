cc6b30ce6e50c3c74e798d4522b0259798096fc9
SrTobi <code.databyte@gmail.com>
2020-05-31 15:05:16 +0200

    Added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,63 @@
+//! This library enables the creation of recursive closures by providing a
+//! single macro [`fix_fn`]. The functionality is similar to the
+//! [Y combinator](https://en.wikipedia.org/wiki/Fixed-point_combinator#Fixed-point_combinators_in_lambda_calculus).
+//! Recursive closures can have arbitrary amounts of parameters and can capture
+//! variables.
+//!
+//! ```
+//! use fix_fn::fix_fn;
+//!
+//! let fib = fix_fn!(|fib, i: u32| -> u32 {
+//!     if i <= 1 {
+//!            i
+//!      } else {
+//!          // fib will call the closure recursively
+//!          fib(i - 1) + fib(i - 2)
+//!      }
+//!  });
+//!
+//! assert_eq!(fib(7), 13);
+//! ```
+//! 
+//! The generated code is not completely abstraction free as it uses one dyn trait
+//! (without any boxing) to overcome rust's recursive type limitations.
+//! In most cases, however, the optimizer should be able to eliminate any dynamic dispatch.
+//! 
+//! Unfortunately, mutable recursive closures are not supported.
+
+
+/// Takes a closure definition where the first parameter will be a [`Fn`] to the closure itself.
+/// Returns a recursive closure with the same signature, except the first parameter will be
+/// eliminated.
+/// 
+/// The passed closure needs to have at least one parameter. This
+/// first parameter can be used to call the closure itself, achieving recursion.
+/// It must not be annotated with a type.
+/// 
+/// Additional parameters will be parameters of the resulting closure.
+/// All additional parameters must be annotated with types.
+/// 
+/// The closure definition needs to have a result-type annotation.
+/// 
+/// `move` can be used and has the [usual semantic](https://doc.rust-lang.org/1.18.0/book/first-edition/closures.html#move-closures).
+/// 
+/// # Example
+/// 
+/// ```
+/// use fix_fn::fix_fn;
+///  
+/// let fib = fix_fn!(|fib, i: u32| -> u32 {
+///     if i <= 1 {
+///         i
+///     } else {
+///         // fib will call the closure recursively
+///         fib(i - 1) + fib(i - 2)
+///     }
+/// });
+///
+/// // resulting lambda only has the `i: u32` parameter
+/// assert_eq!(fib(7), 13);
+/// ```
 #[macro_export]
 macro_rules! fix_fn {
     (
